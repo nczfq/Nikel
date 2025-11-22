0680913609f9bc6c4cb515e10e3a8f6fc84cca38
Felipe Quadros <felipe.quadros.06@gmail.com>
2025-11-21 23:01:19 -0300

    Iniciando o GIT

---
+++ public/js/base.js
@@ -0,0 +1,35 @@
+const nome = 'Felipe Quadros';
+let nome2 = '';
+let pessoa = {
+    nome: 'Marcelo',
+    idade: '33',
+    prof: 'Programador'
+}
+
+
+let nomes = ['marcelo', 'pdro', 'anderson']
+
+
+function alterarNome() {
+    nome2 = 'Maria Silva';
+    console.log('VALOR ALTERADO: ');
+    console.log(nome2);
+}
+
+
+function recebeEalteraNome(novoNome) {
+    nome2 = novoNome
+    console.log('VALOR ALTERADO INPUT: ');
+    console.log(nome2);
+}
+
+
+
+
+//console.log(pessoa.nome);
+
+//recebeEalteraNome('Anderson')
+
+//recebeEalteraNome('Maria Silva')
+
+//altenarNome()
